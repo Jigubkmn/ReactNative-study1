0d19a52bc7f80e361449d07894cd95a38488ef5d
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-10 23:25:22 +0900

    indexページ作成

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      {/* headerShown: falseにすることでタブ内の角ページのヘッダーを非表示 */}
+      <Stack.Screen name="(tabs)" options={{ headerShown: false}} />
+      <Stack.Screen name="+not-found"/>
+    </Stack>
+  )
 }
