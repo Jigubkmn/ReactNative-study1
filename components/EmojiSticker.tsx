21fb77e6b3106681c9260b12cb7a7bc36a3482a0
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-11 23:45:26 +0900

    タップジェスチャーを追加

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,5 +1,7 @@
-import { Image, type ImageSource } from "expo-image";
+import { type ImageSource } from "expo-image";
 import { View } from "react-native";
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
 
 type Props = {
   imageSize: number;
@@ -7,9 +9,34 @@ type Props = {
 }
 
 export function EmojiSticker({imageSize, stickerSource} :Props) {
+  const scaleImage = useSharedValue(imageSize);
+
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = imageSize * 2
+      } else {
+        scaleImage.value = Math.round(scaleImage.value / 2)
+      }
+    })
+
+    // 絵文字ステッカーのサイズを調整
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
   return(
     <View style={{ top: -350 }}>
-      <Image source={stickerSource} style={{ width: imageSize, height: imageSize }} />
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </ GestureDetector>
     </View>
   );
 }
